24593ebc8f939f4ab592f5a9775e0fca990d4586
Alexander <krivoshein.w@gmail.com>
2024-11-22 23:10:41 +0300

    Added a page with organization security policy

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GoodsView from '@/views/GoodsView.vue'
 import AboutView from '@/views/AboutView.vue'
+import PolicyView from '@/views/PolicyView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,12 @@ const router = createRouter({
       component: AboutView,
       props: true,
     },
+    {
+      path: '/policy',
+      name: 'PolicyView',
+      component: PolicyView,
+      props: true,
+    }
   ],
   scrollBehavior(to, from, savedPosition) {
     return {top: 0 };
