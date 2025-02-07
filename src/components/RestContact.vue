e53ec14ee648c283dc23c50b4169b2e6c9afed68
Alexander <krivoshein.w@gmail.com>
2025-02-07 17:05:09 +0300

    Fixes and features #11 Fixed JSON file Implemented adding restaurant contacts from JSON file

--- src/components/RestContact.vue
+++ src/components/RestContact.vue
@@ -1,17 +1,29 @@
 <template>
-<div class="contact">
-  <h3 class="contact__rest">contactOfRest.name</h3>
-<!--  V-for-->
-  <ul class="contact__info">
-    <address class="contact__address">contactOfRest.address</address>
-    <a href="tel:+74873452211" class="contact__phone">contactOfRest.phone</a>
-  </ul>
-</div>
+  <div class="contact">
+    <h3 class="contact__rest">{{ contactOfRest.name }}</h3>
+
+    <ul class="contact__info">
+      <li v-for="(location, index) in contactOfRest.locations" :key="index">
+        <address class="contact__address">
+          {{ location.address }}
+        </address>
+        <a :href="'tel:' + location.phone" class="contact__phone">
+          {{ location.phone }}
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
 
+const props = defineProps({
+  contactOfRest: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
-<style lang="scss">
 
-</style>
+<style lang="scss"></style>
